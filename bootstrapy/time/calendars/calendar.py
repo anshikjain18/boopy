a6ce509e92fd9815d54889b4f83c93daa8a25a9a
Xiar-fatah <xiar.fatah@gmail.com>
2023-05-07 19:28:50 +0200

    Implemented advance for days

--- bootstrapy/time/calendars/calendar.py
+++ bootstrapy/time/calendars/calendar.py
@@ -1,4 +1,5 @@
-
+from bootstrapy.time.calendars.sweden import sweden_is_business_day
+import datetime
 def adjust(date : str,
            convention : str ):
     """
@@ -13,7 +14,7 @@ def adjust(date : str,
         The business day convention, such as following, modified following and such.
     """
     pass
-def advance(date : str,
+def advance(date : datetime.date,
             n : int,
             time_unit : str,
             convention : str):
@@ -30,4 +31,25 @@ def advance(date : str,
         The business day convention, such as following, modified following and such.
 
     """
-    pass
+    if (n == 0):
+        return adjust(date, convention)
+    elif (time_unit == 'D') | (time_unit == 'd'):
+        d1 = date
+        if (n > 0):
+            while (n > 0):
+                d1 = d1 + datetime.timedelta(days=1)
+                while (sweden_is_business_day(d1) != True):
+                    d1 = d1 + datetime.timedelta(days=1)
+                n -= 1
+        else:
+            while (n < 0):
+                d1 = d1 + datetime.timedelta(days = -1)
+                while (sweden_is_business_day(d1) != True):
+                    d1 = d1 + datetime.timedelta(days = -1)
+                n += 1
+        return d1
+    # To be implemented calendar.cpp
+    #elif time_unit == 'W' | time_unit == 'w':
+        #d1 = d + n *unit 
+
+        
