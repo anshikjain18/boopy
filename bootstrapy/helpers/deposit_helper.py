7103a326930329a8707eb55ff06c47684e09e8bf
Xiar-fatah <xiar.fatah@gmail.com>
2023-05-15 19:25:26 +0200

    Working on numerical solver.

--- bootstrapy/helpers/deposit_helper.py
+++ bootstrapy/helpers/deposit_helper.py
@@ -1,11 +1,15 @@
 from bootstrapy.helpers.interest_rate_helper import InterestRateHelper
 from bootstrapy.time.date.maturity import initialize_maturity_date, maturity_datetime, maturity_int
 import bootstrapy.time.date.reference_date as reference_date_holder
-
+from bootstrapy.time.calendars.calendar import advance
 # types
 from typing import Callable
 class DepositHelper(InterestRateHelper):
-    def __init__(self, maturity_input : str, settlement_input: int, daycounter: Callable[[int, int], float]):
+    def __init__(self, 
+                 maturity_input : str, 
+                 settlement_input: int, 
+                 daycounter: Callable[[int, int], float]
+                 ):
         self.timeunit = maturity_input[-1]
         self.length = int(maturity_input[:len(maturity_input)-1])
         self.maturity_date = maturity_datetime(
@@ -13,14 +17,11 @@ class DepositHelper(InterestRateHelper):
             settlement_input 
             )
         self.maturity_days = maturity_int(reference_date_holder.reference_date, self.maturity_date)
-        self.settlement_date = maturity_datetime(
-            initialize_maturity_date(reference_date_holder.reference_date, "D", 
-                                    settlement_input),
-                                    settlement_input)
-        self.settlement_days = maturity_int(reference_date_holder.reference_date, self.settlement_date)
-        self.daycount_time = daycounter(self.settlement_days, self.maturity_days)
 
-        self.value_date = None # Should be given by advancing the calendar.
-    
+        self.value_date = advance(reference_date_holder.reference_date, settlement_input, "D", None)# Should be given by advancing the calendar.
+        self.value_days = maturity_int(reference_date_holder.reference_date, self.value_date)
+        # ? Should just be inserted to the function implied quote
+        #self.daycount_time = daycounter(self.value_days, self.maturity_days)
     def implied_quote(self):
         raise NotImplementedError
+
