a5188de7151fd6b01183e169c62be38aa658e927
Xiar <54110012+Xiar-fatah@users.noreply.github.com>
2023-06-11 23:16:22 +0200

    Working on ibor_index

---
+++ bootstrapy/indices/ibor_index.py
@@ -0,0 +1,14 @@
+from typing import Callable
+class IborIndex:
+    def __init__(self, 
+                 settlement_days : int,
+                 period : str,
+                 calendar : Callable
+                 convention : str,
+                 day_count : Callable):
+        self.settlement_days = settlement_days
+        self.timeunit = period[-1]
+        self.length = int(period[:len(period)-1])
+        self.calendar = calendar
+        self.convention = convention
+        self.day_count = day_count
